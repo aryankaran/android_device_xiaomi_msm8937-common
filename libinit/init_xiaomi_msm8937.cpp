13fc1d9183542a86d7f0249beb86652c694bf54b
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-02-26 16:14:33 +0800

    Revert "TEMP: libinit: Force ADB on boot"
    This reverts commit f2fd5e326a91b7fd6175c530cbd765a70c84d04c.

--- libinit/init_xiaomi_msm8937.cpp
+++ libinit/init_xiaomi_msm8937.cpp
@@ -5,15 +5,9 @@
  */
 
 #include <libinit_dalvik_heap.h>
-#include <libinit_utils.h>
 
 #include "vendor_init.h"
 
 void vendor_load_properties() {
     set_dalvik_heap();
-
-    property_override("ro.secure", "0");
-    property_override("ro.adb.secure", "0");
-    property_override("ro.debuggable", "1");
-    property_override("persist.sys.usb.config", "adb");
 }
